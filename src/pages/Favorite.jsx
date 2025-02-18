71a2721f2935121d8ccf1cee079a2bed10503d0a
Abel Bane <abelbane@gmail.com>
2025-02-17 21:28:25 -0500

    Implemented contexts and localstorage for favorites

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,8 +1,22 @@
+import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../contexts/MovieContext";
+
+
 function Favorite(){
-    return <div className="inline-block p-12 text-white border-none rounded-md bg-[var(--color-secondary)] w-full text-center ">
-        <h2 className="text-red-600"><b>No Favorite Movies Yet</b></h2>
-        <p>Start Adding movies to your favorites</p>
-    </div>
+    const {favorites} = useMovieContext();
+
+    return (
+        <div className="grid w-full gap-8 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 mt-4">
+            {
+                favorites.length ? favorites.map(favorite => <MovieCard key={favorite.id} movie={favorite} />) : 
+                <div className="inline-block p-12 text-white border-none rounded-md bg-[var(--color-secondary)] w-full text-center ">
+                    <h2 className="text-red-600"><b>No Favorite Movies Yet</b></h2>
+                    <p>Start Adding movies to your favorites</p>
+                </div>
+            }
+        </div>
+    );
+    
 
 }
 
